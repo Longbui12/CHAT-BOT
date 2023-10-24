b781f413c95de612826831565aed11ab42e9e0d0
rn25-long-app <bhlong3112@gmail.com>
2023-10-24 15:05:51 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -150,6 +150,10 @@ function handlePostback(sender_psid, received_postback) {
     response = { text: "Thanks!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
+  } else if (payload === "GET_STARTED") {
+    response = {
+      text: "OK ! Xin chào mừng bạn ... đến với nhà hàng của chúng tôi .",
+    };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
