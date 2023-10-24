9aa7ba863cdeae056b98b75d8768873806e7621e
rn25-long-app <bhlong3112@gmail.com>
2023-10-24 17:50:53 +0700

    Update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -29,11 +29,35 @@ let callSendAPI = (sender_psid, response) => {
     }
   );
 };
+let getUserName = async (sender_psid) => {
+  let username = "";
+  // Send the HTTP request to the Messenger Platform
+  await request(
+    {
+      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "GET",
+    },
+    (err, res, body) => {
+      console.log(body);
+      if (!err) {
+        response = JSON.parse(res);
+
+        username = `${response.first_name} ${response.last_name}`;
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+  return username;
+};
+
 let handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
+      let username = await getUserName(sender_psid);
       let response = {
-        text: "OK ! Xin chào mừng bạn ... đến với nhà hàng của chúng tôi .",
+        text: `OK ! Xin chào mừng bạn ${username} đến với nhà hàng của chúng tôi .`,
       };
       await callSendAPI(sender_psid, response);
       resolve("done");
