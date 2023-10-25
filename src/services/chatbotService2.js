e9c6ca915ae527ed33601051abb4aa0c7f9c700a
rn25-long-app <bhlong3112@gmail.com>
2023-10-26 04:53:23 +0700

    update

--- src/services/chatbotService2.js
+++ src/services/chatbotService2.js
@@ -3,6 +3,35 @@ require("dotenv").config();
 
 const IMAGE_DETAIL_ROOMS = "https://bit.ly/long-bot1";
 
+let callSendAPI = async (sender_psid, response) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  await sendMarkReadMessage(sender_psid);
+  await sendTypingOn(sender_psid);
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v18.0/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+};
 let getImageRoomsTemplate = () => {
   let response = {
     attachment: {
