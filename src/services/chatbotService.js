7850da228dc468b84d1757828f102c4c04024412
rn25-long-app <bhlong3112@gmail.com>
2023-10-26 04:02:51 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -45,7 +45,7 @@ const IMAGE_DETAIL_MEAT_3 =
 
 // Handle function
 
-let callSendAPI = (sender_psid, response) => {
+let callSendAPI = async (sender_psid, response) => {
   // Construct the message body
   let request_body = {
     recipient: {
@@ -53,6 +53,9 @@ let callSendAPI = (sender_psid, response) => {
     },
     message: response,
   };
+
+  await sendMarkReadMessage(sender_psid);
+  await sendTypingOn(sender_psid);
 
   // Send the HTTP request to the Messenger Platform
   request(
@@ -71,6 +74,61 @@ let callSendAPI = (sender_psid, response) => {
     }
   );
 };
+
+let sendTypingOn = (sender_psid) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    sender_action: "typing_on",
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v18.0/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("sendTypingOn sent!");
+      } else {
+        console.error("Unable to send sendTypingOn :" + err);
+      }
+    }
+  );
+};
+
+let sendMarkReadMessage = (sender_psid) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    sender_action: "mark_seen",
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v18.0/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("sendTypingOn sent!");
+      } else {
+        console.error("Unable to send sendTypingOn :" + err);
+      }
+    }
+  );
+};
+
 let getUserName = (sender_psid) => {
   return new Promise((resolve, reject) => {
     // Send the HTTP request to the Messenger Platform
